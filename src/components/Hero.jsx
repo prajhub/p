0d2d55e83b4b7b51e0e74561f610a257efe27151
Prajwal Magar <pzlmagar@gmail.com>
2023-02-24 20:09:23 +0545

    Hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,10 +11,19 @@ const Hero = () => {
   return(
   
   <section className='lg:h-[900px] py-12'>
-    <div className='container mx-auto'>
-        <div>
-          <div>
-            <h1>{title}</h1>
+    
+    <div className='container mx-auto  h-full relative'>
+        <div className=' flex flex-col xl:flex-row items-center h-full md:py-24'>
+
+
+          <div className='text-center xl:text-left xl:absolute'>
+            <h1 className='h1 xl:max-w-[700px] mb-6 xl:mb-12' data-aos='fade-down' data-aos-delay='400'>{title}</h1>
+            <p className='lead xl:max-w-[380px] mb-6 lg:mb-12 ' data-aos='fade-down' data-aos-delay='500'>{subtitle}</p>
+            <button className='btn btn-primary mb-8 xl:mb-0' data-aos='fade-down' data-aos-delay='600'>{btnText}</button>
+          </div>
+
+          <div className='xl:absolute xl:-right-12 xl:bottom-16' data-aos='fade-up' data-aos-delay='650'>
+            <img src={image} alt=''/>
           </div>
         </div>
     </div>
